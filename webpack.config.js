f53c3e3ad1ceecc3ebed8cfa8dd932a24309a34c
Akashdeep Samantra <akash.samantra19@gmail.com>
2020-04-24 23:13:09 +0530

    Webpack configuration for react

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const ENV = {
+  DEVELOPMENT: "development",
+  PRODUCTION: "production"
+}
+
+module.exports = () => {
+  const isProduction = process.env && process.env.RUN_ENV && process.env.RUN_ENV === ENV.PRODUCTION
+
+  return {
+    mode: isProduction ? ENV.PRODUCTION : ENV.DEVELOPMENT,
+    entry: "./src/js/index.js",
+    resolve: { extensions: [".js", ".jsx"] },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: "html-loader"
+          }
+
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ],
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: '[name].bundle.js'
+    }
+  };
+};
